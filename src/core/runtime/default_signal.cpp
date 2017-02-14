ed9f48b0645dc16829a4c76bba36b04f351c1859
J M Dieterich <dieterich@ogolem.org>
2017-02-14 10:49:00 -0500

    Expects value not pointer.

--- src/core/runtime/default_signal.cpp
+++ src/core/runtime/default_signal.cpp
@@ -50,7 +50,7 @@ int DefaultSignal::rtti_id_ = 0;
 DefaultSignal::DefaultSignal(hsa_signal_value_t initial_value)
     : Signal(initial_value) {
   signal_.kind = AMD_SIGNAL_KIND_USER;
-  signal_.event_mailbox_ptr = NULL;
+  signal_.event_mailbox_ptr = 0;
   HSA::hsa_memory_register(this, sizeof(DefaultSignal));
 }
 
